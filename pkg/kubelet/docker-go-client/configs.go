23cdf0832e3ac7a92196d63f16c15315f4779967
JunchenLu417 <daniellucs2002@icloud.com>
2025-02-23 00:35:29 +0000

    create.go container utilities

---
+++ pkg/kubelet/docker-go-client/configs.go
@@ -0,0 +1,1 @@
+package docker_go_client
--- pkg/kubelet/docker-go-client/create.go
+++ pkg/kubelet/docker-go-client/create.go
@@ -1,7 +1,86 @@
 package docker_go_client
 
-import "github.com/docker/docker/client"
+import (
+	"context"
+	"fmt"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/errdefs"
+	"io"
+	"log"
+)
 
-func GetNewClient() (*client.Client, error) {
+func getNewClient() (*client.Client, error) {
 	return client.NewClientWithOpts(client.WithVersion("1.47"))
+}
+
+func pullImage(client *client.Client, name string) error {
+	// Check if the image exists locally
+	_, _, err := client.ImageInspectWithRaw(context.Background(), name)
+	if err == nil {
+		log.Printf("Image %s already exists locally, skipping pull.", name)
+		return nil
+	}
+
+	// If the image is not found, pull it
+	if errdefs.IsNotFound(err) {
+		log.Printf("Image %s not found locally, pulling...", name)
+		pull, err := client.ImagePull(context.Background(), name, image.PullOptions{})
+		if err != nil {
+			return err
+		}
+		defer func(pull io.ReadCloser) {
+			err := pull.Close()
+			if err != nil {
+				log.Printf("failed to close image pull response: %v", err)
+			}
+		}(pull)
+	} else {
+		return err
+	}
+	return nil
+}
+
+func createContainer(client *client.Client, containerConfig *container.Config, hostConfig *container.HostConfig,
+	networkingConfig *network.NetworkingConfig, name string) (string, error) {
+
+	create, err := client.ContainerCreate(context.Background(), containerConfig, hostConfig, networkingConfig, nil, name)
+	if err != nil {
+		return "", fmt.Errorf("failed to create container: %w", err)
+	}
+	log.Printf("Container %s created with ID: %s", name, create.ID)
+	return create.ID, nil
+}
+
+func startContainer(client *client.Client, id string) error {
+	err := client.ContainerStart(context.Background(), id, container.StartOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to start container: %w", err)
+	}
+	return nil
+}
+
+// RunContainer should be the only exported symbol (visible and invoked outside the package)
+func RunContainer(containerName string, containerConfig *container.Config,
+	hostConfig *container.HostConfig, networkingConfig *network.NetworkingConfig) error {
+
+	cli, err := getNewClient()
+	if err != nil {
+		return err
+	}
+	err = pullImage(cli, containerConfig.Image)
+	if err != nil {
+		return err
+	}
+	containerID, err := createContainer(cli, containerConfig, hostConfig, networkingConfig, containerName)
+	if err != nil {
+		return err
+	}
+	err = startContainer(cli, containerID)
+	if err != nil {
+		return err
+	}
+	return nil
 }
